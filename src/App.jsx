aa214b2acf8418846e2b2a3a0d892308d1a68e43
rahidulislampritom <rahidulislampritom@gmail.com>
2025-02-09 18:10:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 text-8xl font-bold text-center'>Vite + React</h1>
+     
+    </>
+  )
+}
+
+export default App
